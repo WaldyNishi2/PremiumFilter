b6e4fa795292f9c5d6d6b8caaf831ec3de0a8376
WaldyNishi2 <138442977+WaldyNishi2@users.noreply.github.com>
2023-08-24 18:09:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,7 +67,7 @@ HOW_TO_DOWNLOAD =  environ.get('HOW_TO_DOWNLOAD', 'https://t.me/Sharath_Links/13
 
 AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 100))
 
-FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL'))
+FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', '-1001941031458'))
 
 SHORTNER_SITE =  environ.get('SHORTNER_SITE', '') #Put Only Shortner Site domain don't put like this https://tnlink.in/
 
